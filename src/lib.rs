254267976da6f21eaf955a5cc9a4d6a9c42b504f
viveknathani <viveknathani2402@gmail.com>
2024-11-14 22:54:20 +0530

    feat: accept any argument

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,60 @@
+use bincode;
+use serde::Deserialize;
+use serde::Serialize;
 use std::collections::HashMap;
+use std::sync::Arc;
 
 pub struct OnEventListener {
-    pub callback: Box<dyn Fn() + Send + Sync + 'static>,
-}
-
-#[derive(Default)]
-pub struct OnEvent {
-    listeners: HashMap<String, Vec<OnEventListener>>,
+    callback: Arc<dyn Fn(Vec<u8>) + Sync + Send + 'static>,
 }
 
 /// This is the main entity for managing events in your application.
 /// Each instance will maintain its list of listeners and trigger them on demand.
+pub struct OnEvent {
+    listeners: HashMap<String, Vec<OnEventListener>>,
+}
+
 impl OnEvent {
     pub fn new() -> Self {
-        Default::default()
+        Self {
+            listeners: HashMap::new(),
+        }
     }
 
-    pub fn on<F>(&mut self, event: &str, callback: F)
+    /// Registers an event listener with a callback that takes serialized data.
+    pub fn on<F, T>(&mut self, event: &str, callback: F)
     where
-        F: Fn() + Send + Sync + 'static,
+        for<'de> T: Deserialize<'de>,
+        F: Fn(T) + 'static + Sync + Send,
     {
+        // Wrap the callback to handle deserialization from `Vec<u8>`
+        let parsed_callback = move |bytes: Vec<u8>| {
+            let value: T = bincode::deserialize(&bytes).unwrap();
+            callback(value);
+        };
+
         self.listeners
             .entry(event.to_string())
             .or_default()
             .push(OnEventListener {
-                callback: Box::new(callback),
+                callback: Arc::new(parsed_callback),
             });
     }
 
-    pub fn emit(&mut self, event: &str) {
-        let listeners = self.listeners.get(event);
-        if listeners.is_some() {
-            for listener in listeners.unwrap() {
-                (listener.callback)();
+    /// Emits an event, serializing the provided arguments.
+    pub fn emit<T>(&self, event: &str, args: T)
+    where
+        T: Serialize,
+    {
+        if let Some(listeners) = self.listeners.get(event) {
+            // Serialize the arguments to `Vec<u8>`
+            if let Ok(serialized_args) = bincode::serialize(&args) {
+                for listener in listeners {
+                    let callback = Arc::clone(&listener.callback);
+                    callback(serialized_args.clone());
+                }
+            } else {
+                eprintln!("Failed to serialize arguments for event `{}`", event);
             }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 use onevent::OnEvent;
+use serde::{Deserialize, Serialize};
 
 fn main() {
     let mut emitter = OnEvent::new();
 
-    emitter.on("one", || {
-        println!("i like one");
-    });
+    emitter.on("add", |number: f32| println!("{}", number + 3.0));
+    emitter.emit("add", 5.0 as f32);
+    emitter.emit("add", 4.0 as f32);
 
-    emitter.emit("one");
-    emitter.emit("two");
-    emitter.emit("one");
+    #[derive(Serialize, Deserialize)]
+    struct Options {
+        value: f32,
+    }
+    emitter.on("subtract", |options: Options| {
+        println!("{}", options.value - 3.0)
+    });
+    emitter.emit("subtract", Options { value: 5.0 });
+    emitter.emit("subtract", Options { value: 4.0 });
 }
